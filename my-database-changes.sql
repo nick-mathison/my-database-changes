1b4c01ebb8081a1206e94c5e3f86898c928146cc
Nick Mathison <114104601+nick-mathison@users.noreply.github.com>
2025-06-05 18:08:08 -0400

    Update my-database-changes.sql

--- my-database-changes.sql
+++ my-database-changes.sql
@@ -2,10 +2,10 @@
 
 --changeset matteo:1 labels:create_table
 --comment: Table creation
--- CREATE TABLE doctors( 
---    doctor_id INT GENERATED ALWAYS AS IDENTITY, 
---    first_name VARCHAR(80) NOT NULL, 
---    last_name VARCHAR(80) NOT NULL 
--- ); 
+CREATE TABLE doctors( 
+   doctor_id INT GENERATED ALWAYS AS IDENTITY, 
+   first_name VARCHAR(80) NOT NULL, 
+   last_name VARCHAR(80) NOT NULL 
+); 
 rollback 
 DROP TABLE IF EXISTS doctors; 
