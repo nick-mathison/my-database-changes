e045e24a03502ad0e8af1cee2d9f9e909ec0fa85
Nick Mathison <114104601+nick-mathison@users.noreply.github.com>
2025-06-05 16:29:52 -0400

    Create my-database-changes.sql

---
+++ my-database-changes.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset matteo:1 labels:create_table
+--comment: Table creation
+CREATE TABLE doctors( 
+   doctor_id INT GENERATED ALWAYS AS IDENTITY, 
+   first_name VARCHAR(80) NOT NULL, 
+   last_name VARCHAR(80) NOT NULL 
+); 
+--rollback DROP TABLE IF EXISTS doctors; 
